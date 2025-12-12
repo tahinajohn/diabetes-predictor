1330a003bf4703f1627e08d3ad90da6525cc08a6
tahinajohn <tahina021@gmail.com>
2025-12-12 09:56:17 +0300

    model eval

--- train.py
+++
@@ -1,7 +0,0 @@
-# from model import diabetes_model
-
-# from processing import split_data, X, y
-
-# def train_model(model):
-#     #X_train, X_test, y_train, y_test = split_data(X, y)
-#     model.fit(X, y)
